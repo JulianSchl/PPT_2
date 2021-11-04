401223d59c3a8367c1315655503f0184a7aa1209
JulianSchl <83708903+JulianSchl@users.noreply.github.com>
2021-11-04 14:25:39 +0100

    Update run.py

--- mbdyn/run.py
+++ mbdyn/run.py
@@ -1,5 +1,5 @@
 import logging
-#from matplotlib import pyplot as plt 
+#from matplotlib import pyplot as plt
 import precice
 import time
 import subprocess
@@ -13,6 +13,7 @@ import socket
 import errno
 from scipy import interpolate
 import colorama
+import struct
 from .socketTools import *# SocketTools as s
 
 class MbdynAdapter:
@@ -35,8 +36,7 @@ class MbdynAdapter:
 		for i in range(self.patches):
 			self.patch.append(Rotation())
 			XA, YA, ZA = np.hsplit(csvImport(self.current_path + '/../' + self.fluid_folder_name + '/patch' + str(i+1) + '.csv'),3)	#if mesh is in file
-			self.patch[i].importGrid(csvImport(self.current_path + '/../' + self.fluid_folder_name + '/patch' + str(i+1) + '.csv'))
-			#import ipdb; ipdb.set_trace()
+			self.patch[i].importGrid(XA,YA,ZA)
 			self.vertices.append(np.array([XA.flatten(),YA.flatten(),ZA.flatten()]).T)
 			
 		if not (len(self.vertices) == len(self.mesh_id)):
@@ -70,14 +70,39 @@ class MbdynAdapter:
 			self.read_data_id.append(interface.get_data_id(self.read_data_name[i], self.mesh_id[i]))
 			self.write_data_id.append(interface.get_data_id(self.write_data_name[i], self.mesh_id[i]))
 		
+		self.mbdyn.initialize(case=self.mbdyn_prep.name)
+		
 		print("trying to connect to socket stream...")
+		#MBDyn drive caller socket
+		self.tvc_socket = s.driveConnect()
 		self.exchange_socket = s.patchConnect(self.patches+1)
 		print(u'\u2705' + " " + colorama.Style.BRIGHT + colorama.Back.WHITE + colorama.Fore.GREEN + str(self.patches+2) + " socket(s) connected")
 		
 		dt = interface.initialize()
 		
 		self.input.update_time_step(dt)
-		self.mbdyn.initialize(case=self.mbdyn_prep.name)
+		
+		print("preInit")
+		self.mbdyn.nodal_initialize()
+		#send tvc socket data
+		s_out_bufsize = 8
+		i = 0
+		#tvc =  self.iteration * 0.02
+		tvc = 0
+		buf_out = bytearray(struct.pack("d",tvc))
+		self.tvc_socket[i].sendall(buf_out, s_out_bufsize)
+		print("tvc: " + str(tvc))
+		#send tvc socket data
+		i = 0
+		#tvc =  self.iteration * 0.02
+		tvc = 0
+		buf_out = bytearray(struct.pack("d",tvc))
+		self.tvc_socket[i].sendall(buf_out, s_out_bufsize)
+		print("tvc: " + str(tvc))
+		self.mbdyn.nodal_negotiate()
+		#TODO: Is first iteration step here?
+		self.mbdyn.nodal_negotiate_recv()
+
 
 		self.transform = Rotation()
 		
@@ -108,113 +133,170 @@ class MbdynAdapter:
 			#print(np.vstack(self.force_tensor))
 			#import ipdb; ipdb.set_trace()
 			self.mbdyn.set_forces(np.vstack(self.force_tensor))
+			
+			target_tvc_angle = 0
+			tvc_speed = 1*10**(-1.8)
+			if float(self.current_time_step) >= 0.25 and float(self.current_time_step) < 0.5:
+				target_tvc_angle = np.pi/4
+			elif float(self.current_time_step) >= 0.5 and float(self.current_time_step) < 0.75:
+				target_tvc_angle = 0
+			elif float(self.current_time_step) >= 0.75 and float(self.current_time_step) < 1:
+				target_tvc_angle = -np.pi/4
+			elif float(self.current_time_step) >= 1 and float(self.current_time_step) < 1.25:
+				target_tvc_angle = np.pi/2
+			elif float(self.current_time_step) >= 1.25:
+				target_tvc_angle = -np.pi/2
+			
+			if target_tvc_angle > tvc and not target_tvc_angle < tvc + tvc_speed:
+				tvc = tvc + tvc_speed
+			elif target_tvc_angle < tvc and not target_tvc_angle > tvc - tvc_speed:
+				tvc = tvc - tvc_speed
+			#import ipdb; ipdb.set_trace()
+			#tvc = self.iteration * 0.02
+			
+			#send tvc socket data
+			i = 0
+			buf_out = bytearray(struct.pack("d",tvc))
+			self.tvc_socket[i].sendall(buf_out, s_out_bufsize)
+			print("\ntvc: " + str(tvc))
+			print("\ntarget tvc angle: " + str(target_tvc_angle))
+			
 			if self.mbdyn.solve(False):
 				self.module_logger.debug('Something went wrong!')
 				#MBDyn diverges
 				break
 				
+			#displacement_real = self.mbdyn.get_absolute_displacement()
+			#displacement = self.mbdyn.get_absolute_displacement(False,True)
+			#import ipdb; ipdb.set_trace()
 			#nodes per patch
 			npp = self.mbdyn.mesh.number_of_nodes()/self.patches
 			if npp % 2 != 0 and npp % 1 != 0:
 				exit("please check number of nodes / patches!")
 			npp = int(npp)
-			
+			#import ipdb; ipdb.set_trace()
 			self.displacement_absolute.clear()
 			self.displacement.clear()
 			self.rotation.clear()
 			self.message.clear()
 			self.centre_of_gravity.clear()
 			
-			self.displacement_absolute_delta.clear()
-			self.displacement_absolute_delta_str.clear()
-			self.displacement_relative_delta.clear()
-			self.rotation_absolute_delta.clear()
-			self.rotation_absolute_delta_str.clear()
-			self.mbdyn_mesh.clear()
-			self.mbdyn_mesh_str.clear()
-			self.nodes_absolute.clear()
-			self.message.clear()
+			i=0
+			self.displacement_absolute.append(self.mbdyn.get_absolute_displacement()[i*npp:(i+1)*npp,:].copy())
+			self.rotation.append(self.mbdyn.get_rotation()[i*npp:(i+1)*npp,:].copy())
 			
+			self.message.append("{:.4f}".format(self.displacement_absolute[i][npp-1+i*npp,0]) + ";")
+			self.message[i] += "{:.4f}".format(self.displacement_absolute[i][npp-1+i*npp,1]) + ";"
+			self.message[i] += "{:.4f}".format(self.displacement_absolute[i][npp-1+i*npp,2]) + ";"
+			self.message[i] += "{:.4f}".format(self.rotation[i][npp-1+i*npp,0]*0) + ";"
+			self.message[i] += "{:.4f}".format(self.rotation[i][npp-1+i*npp,1]*0) + ";"
+
+			#self.message[i] += "{:.4f}".format(tvc)
+			
+			#no Farfield rotation
+			self.message[i] += "{:.4f}".format(tvc*0)
+			
+			self.displacement_absolute.clear()
+			self.rotation.clear()
 			for i in range(self.patches):
 				
-				# absolute = MBDyn base mesh as reference
-				# absolute delta = MBDyn base mesh as reference, difference between current pos and base mesh
-				#evaluate difference between initial mesh and current mesh
-				#TODO: add clear commands above
-				self.displacement_absolute_delta.append(self.mbdyn.get_absolute_displacement()[i*npp:(i+1)*npp,:]) 
-				self.displacement_absolute_delta_str.append(np.array(np.array(self.displacement_absolute_delta[i],dtype='float16'),dtype='<U16'))
-				self.rotation_absolute_delta.append(self.mbdyn.get_rotation()[i*npp:(i+1)*npp,:])
-				self.rotation_absolute_delta_str.append(np.array(np.array(self.rotation_absolute_delta[i],dtype='float16'),dtype='<U16'))
-				#TODO: change to old version with dynamicMeshDict
-				self.mbdyn_mesh.append(self.mbdyn.mesh.nodes[i*npp:(i+1)*npp,:])
-				self.mbdyn_mesh_str.append(np.array(np.array(self.mbdyn_mesh[i],dtype='float16'),dtype='<U16'))
-				self.nodes_absolute.append(self.mbdyn.get_nodes()[i*npp:(i+1)*npp,:])
-				self.nodes_absolute_str.append(np.array(np.array(self.nodes_absolute[i],dtype='float16'),dtype='<U16'))
-			
-			##############################
-			### evaluate OpenFOAM data ###
-			##############################
-			for i in range(self.patches+1):
-				#distinction between background and overset mesh
+				#displacement
+				#TODO: change reference system of rotation -> to ground_2 / ground_3
+				#
+				self.displacement_absolute.append(self.mbdyn.get_absolute_displacement()[i*npp:(i+1)*npp,:].copy())
+				
+				#TODO: change rot Point? No -> its static
+				#transform rotor rotation of most deformed element
+				#TODO: move CofG of mbyn.get_nodes, delete changes at rot center below
+				#import ipdb; ipdb.set_trace()
+				self.transform.importGrid(
+				self.mbdyn.get_nodes()[i*npp:npp-2+i*npp,0]-self.mbdyn.get_nodes()[npp-1+i*npp,0]+self.mbdyn.mesh.nodes[npp-1+i*npp,0],
+				self.mbdyn.get_nodes()[i*npp:npp-2+i*npp,1]-self.mbdyn.get_nodes()[npp-1+i*npp,1]+self.mbdyn.mesh.nodes[npp-1+i*npp,1],
+				self.mbdyn.get_nodes()[i*npp:npp-2+i*npp,2]-self.mbdyn.get_nodes()[npp-1+i*npp,2]+self.mbdyn.mesh.nodes[npp-1+i*npp,2],)
+				XA, YA, ZA = self.transform.rotate(angle=np.degrees(float(-self.mbdyn.get_rotation()[npp-2+i*npp,2])),rot_point = np.array((0,0,0)))
+				self.transform.importGrid(XA,YA,ZA)
+				#TODO: why npp-2 in rotation? ... already corrected for CG in message
+				#-> delta between deformation and oscillation
+				XA, YA, ZA = self.transform.rotate(angle=np.degrees(float(-self.mbdyn.get_rotation()[5+i*npp,2]+self.mbdyn.get_rotation()[npp-2+i*npp,2])),rot_point = self.mbdyn.mesh.nodes[npp-2+i*npp,:])
+				self.displacement.append((np.array((XA.flatten(),YA.flatten(),ZA.flatten())).T.copy() - self.mbdyn.mesh.nodes[i*npp:npp-2+i*npp,:]).copy())
+
+				
+				# centre of gravity
+				#import ipdb; ipdb.set_trace()
+				# i == 0 (rechts)
+				# i == 1 (links)
+				#import ipdb; ipdb.set_trace()
 				if i == 0:
-					#background mesh
-					#import ipdb; ipdb.set_trace()
-					self.message.append(';'.join(np.concatenate((self.displacement_absolute_delta_str[i][npp-1,:],
-																	np.array(np.array(self.rotation_absolute_delta[i][npp-1,:]*0,dtype='float16'),dtype='<U16'),
-																	np.array(np.array(self.mbdyn_mesh[i][npp-1,:]*0,dtype='float16'),dtype='<U16')))))
+					self.centre_of_gravity.append(np.array((0.5,0,0)))
 				else:
-					#overset mesh
-					self.message.append(';'.join(np.concatenate((self.displacement_absolute_delta_str[i-1][5,:],
-																	self.rotation_absolute_delta_str[i-1][5,:],
-																	self.mbdyn_mesh_str[i-1][5,:]))))
+					self.centre_of_gravity.append(np.array((-0.5,0,0)))
+				#self.centre_of_gravity.append(self.mbdyn.get_nodes()[(i+1)*npp-1])
+				#import ipdb; ipdb.set_trace()
+				### Rotation um z achse: -pi ... pi
+				self.rotation.append(self.mbdyn.get_rotation()[i*npp:(i+1)*npp,:].copy())
+				#import ipdb; ipdb.set_trace()
+				
+				#displacement is independent of CofG
+				#rotation must be just oscillation #TODO
+				#TODO: Change CofG to Anchor point 12
+				#TODO: Adjust MBDyn to Paraview centre
+				
+				self.message.append("{:.4f}".format(self.displacement_absolute[i][5,0]) + ";")
+				self.message[i+1] += "{:.4f}".format(self.displacement_absolute[i][5,1]) + ";"
+				self.message[i+1] += "{:.4f}".format(self.displacement_absolute[i][5,2]) + ";"
+				self.message[i+1] += "{:.4f}".format(self.rotation[i][5,0]*0) + ";"
+				self.message[i+1] += "{:.4f}".format(self.rotation[i][5,1]*0) + ";"
+				self.message[i+1] += "{:.4f}".format(self.mbdyn.get_rotation()[5+i*npp,2]-self.mbdyn.get_rotation()[npp-1+i*npp,2])
+				
+				'''self.message.append(str(float(0)) + ";")
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0)) + ";"
+				self.message[i] += str(float(0))'''
+			
+			for i in range(self.patches+1):
 				print('Sending')
 				print(self.message[i])
-				print("iteration:" +str(i))
 				self.exchange_socket[i].sendall(str.encode(self.message[i]))
-					
 				print('Sent')
-				data = self.exchange_socket[i].recv(190) # 21 chars per message
+				data = self.exchange_socket[i].recv(150)
 				if str(data) == "b''":
-					for j in range(self.patches+1):
-						self.exchange_socket[j].close()
+					self.exchange_socket[i].close()
 					exit("Client exit")
 				split_string = str(data).split("\\n")
 				data = split_string[0][2:]
 				if str(data) == "exit":
-					for j in range(self.patches+1):
+					for j in range(self.patches):
 						self.exchange_socket[j].close()
 					exit("Client exit")
-				print('Received')
-				#print(repr(data))
-			
-			#############################
-			### evaluate preCICE data ###
-			#############################
-			#time.sleep(2)
-			for i in range(self.patches):				
-				#transform rotor rotation of most deformed element
-				
-				#new anchor node positions - ground displacement_absolute_delta
-				#TODO: npp-1,: instead npp-2,:
-				#import ipdb; ipdb.set_trace()
-				self.transform.importGrid(self.nodes_absolute[i][:npp-2,:]-self.displacement_absolute_delta[i][npp-1,:])
-				XA, YA, ZA = self.transform.rotate(angle=-self.rotation_absolute_delta[i][npp-2,2],rot_point = np.array((0,0,0)))
-				self.transform.importGrid(np.array((XA,YA,ZA)).T)
-				#TODO: why npp-2 in rotation?
-				#-> delta between deformation and oscillation
-				XA, YA, ZA = self.transform.rotate(angle=-self.rotation_absolute_delta[i][5,2]+self.rotation_absolute_delta[i][npp-2,2], rot_point = self.mbdyn_mesh[i][npp-2,:])
-				self.displacement_relative_delta.append((np.array((XA.flatten(),YA.flatten(),ZA.flatten())).T.copy() - self.mbdyn_mesh[i][:npp-2,:]))
-				
+				print('Received', repr(data))
+	
 			self.write_data.clear()
 			for i in range(self.patches):
-				#self.write_data.append(np.concatenate(np.array((self.displacement[i][:11,:],self.displacement[i][:11,:]))))
+				#mesh_displacement = np.zeros((num_of_points,2))
+				mesh_displacement = np.concatenate(np.array((self.displacement[i][:11,:],self.displacement[i][:11,:])),axis=0)
+				#TODO: preCICE issue: undefined motion
+				#self.write_data.append(mesh_displacement)
+				self.write_data_tmp = np.array((mesh_displacement))
+				#import ipdb;ipdb.set_trace()
 				#transform coordinate system (because of the farfield rotation)
-				self.transform.importGrid(np.concatenate(np.array((self.displacement_relative_delta[i][:11,:],self.displacement_relative_delta[i][:11,:]))))				
-				self.write_data.append(self.transform.rotate(angle=self.rotation_absolute_delta[i][5,2]).T)
+				self.transform.importGrid(self.write_data_tmp[:,0],self.write_data_tmp[:,1],self.write_data_tmp[:,2])
+				XA, YA, ZA = self.transform.rotate(angle=np.degrees(float(self.rotation[i][5,2])))
+				print("rotation angle (oscillation):")
+				print(np.degrees(float(self.rotation[i][5,2])))
+				print("rotation angle (rotor):")
+				print(np.degrees(float(self.rotation[i][11,2])))
+				
+				self.write_data.append((np.array((XA.flatten(),YA.flatten(),ZA.flatten())).T).copy())
+				
 				
 				#No preCICE
-				#self.write_data.append(np.array((XA.flatten()*0,YA.flatten()*0,ZA.flatten()*0)).T)
-				
+				#self.write_data.append((np.array((XA.flatten()*0,YA.flatten()*0,ZA.flatten()*0)).T).copy())
+
 			
 			if interface.is_write_data_required(dt):
 				for i in range(self.patches):
